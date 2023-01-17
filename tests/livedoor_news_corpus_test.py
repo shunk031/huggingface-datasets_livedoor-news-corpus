dd4faf768fb2a92f5eb93b18ef87c02485d87c4c
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-01-18 00:37:57 +0900

    update test

--- tests/livedoor_news_corpus_test.py
+++ tests/livedoor_news_corpus_test.py
@@ -12,3 +12,44 @@ def test_load_dataset(dataset_path: str):
 
     assert dataset["train"].num_rows == 7367  # type: ignore
     assert len(set(dataset["train"]["category"])) == 9  # type: ignore
+
+
+@pytest.mark.parametrize(
+    argnames="tng_ratio, val_ratio, tst_ratio,",
+    argvalues=(
+        (80, 10, 10),
+        (60, 20, 20),
+    ),
+)
+def test_train_valid_test_split(
+    dataset_path: str,
+    tng_ratio: float,
+    val_ratio: float,
+    tst_ratio: float,
+    num_overall_dataset: int = 7367,
+):
+    assert tng_ratio + val_ratio + tst_ratio == 100
+
+    tng_dataset = ds.load_dataset(
+        path=dataset_path,
+        split=f"train[:{tng_ratio}%]",
+    )
+    val_dataset = ds.load_dataset(
+        path=dataset_path,
+        split=f"train[{tng_ratio}%:{tng_ratio + val_ratio}%]",
+    )
+    tst_dataset = ds.load_dataset(
+        path=dataset_path,
+        split=f"train[{tng_ratio + val_ratio}%:{tng_ratio + val_ratio + tst_ratio}%]",
+    )
+
+    dataset = ds.DatasetDict(
+        {"train": tng_dataset, "validation": val_dataset, "test": tst_dataset}
+    )
+
+    assert (
+        num_overall_dataset
+        == dataset["train"].num_rows
+        + dataset["validation"].num_rows
+        + dataset["test"].num_rows
+    )
