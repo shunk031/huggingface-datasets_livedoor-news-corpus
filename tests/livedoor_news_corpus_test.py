ab3126cd25556f7fc5ab29566547e88b2db82a1d
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-01-18 01:00:30 +0900

    update test

--- tests/livedoor_news_corpus_test.py
+++ tests/livedoor_news_corpus_test.py
@@ -17,8 +17,8 @@ def test_load_dataset(dataset_path: str):
 @pytest.mark.parametrize(
     argnames="tng_ratio, val_ratio, tst_ratio,",
     argvalues=(
-        (80, 10, 10),
-        (60, 20, 20),
+        (0.8, 0.1, 0.1),
+        (0.6, 0.2, 0.2),
     ),
 )
 def test_train_valid_test_split(
@@ -26,8 +26,51 @@ def test_train_valid_test_split(
     tng_ratio: float,
     val_ratio: float,
     tst_ratio: float,
+):
+    assert tng_ratio + val_ratio + tst_ratio == 1.0
+    original_dataset = ds.load_dataset(path=dataset_path, split="train")
+    original_dataset = original_dataset.shuffle(seed=42)
+
+    # split train and validation + test
+    tng_valtst_dataset = original_dataset.train_test_split(  # type: ignore
+        train_size=tng_ratio,
+    )
+    # then, split validation + test to validation and test
+    val_tst_dataset = tng_valtst_dataset["test"].train_test_split(
+        test_size=tst_ratio / (val_ratio + tst_ratio)
+    )
+
+    dataset = ds.DatasetDict(
+        {
+            "train": tng_valtst_dataset["train"],
+            "validation": val_tst_dataset["train"],
+            "test": val_tst_dataset["test"],
+        }
+    )
+
+    assert (
+        len(original_dataset)  # type: ignore
+        == dataset["train"].num_rows
+        + dataset["validation"].num_rows
+        + dataset["test"].num_rows
+    )
+
+
+@pytest.mark.parametrize(
+    argnames="tng_ratio, val_ratio, tst_ratio,",
+    argvalues=(
+        (80, 10, 10),
+        (60, 20, 20),
+    ),
+)
+def test_train_valid_test_split_percent_slicing(
+    dataset_path: str,
+    tng_ratio: float,
+    val_ratio: float,
+    tst_ratio: float,
     num_overall_dataset: int = 7367,
 ):
+
     assert tng_ratio + val_ratio + tst_ratio == 100
 
     tng_dataset = ds.load_dataset(
